ba9c64efdcbd99b7ea678b735100681e8f09dfa9
kenned-candido <kenned_candido@protonmail.com>
2025-09-09 18:28:16 -0300

    Change in emoji markdown

--- commands/util/info.js
+++ commands/util/info.js
@@ -12,8 +12,8 @@ module.exports = {
       .setDescription('Me chamo **Source-chan**! Estou aqui para ajudar, divertir e interagir com todos vocês!')
       .addFields(
         { name: 'Versão', value: config.version, inline: true },
-        { name: 'Linguagem', value: '<:1415050954465874000:1415051460684550174> JavaScript', inline: true },
-        { name: 'Framework', value: `<:1415050963420577952:1415051451469926470> Discord.js v${pkg.dependencies['discord.js'].replace('^', '')}`, inline: true },
+        { name: 'Linguagem', value: '<:1415050954465874000:1415082842517405797> JavaScript', inline: true },
+        { name: 'Framework', value: `<:1415050963420577952:1415082853854613545> Discord.js v${pkg.dependencies['discord.js'].replace('^', '')}`, inline: true },
         { name: 'Desenvolvedor', value: `<@${config.ownerId}>`, inline: false }
       )
       .setColor(config.embedColor)
@@ -24,7 +24,7 @@ module.exports = {
     const row = new ActionRowBuilder().addComponents(
       new ButtonBuilder()
         .setLabel('Github')
-        .setEmoji('<:1415055606850523317:1415055637678788718>')
+        .setEmoji('<:1415055637678788718:1415083270789660702>')
         .setStyle(ButtonStyle.Link)
         .setURL('https://github.com/kenned-candido/source-chan'),
     );
