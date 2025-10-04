677b24261edccf8d8b06cd5ae77f29c2d70a327c
kenned-candido <kenned_candido@protonmail.com>
2025-10-04 19:42:33 -0300

    obsolete and absolute-cinema

---
+++ commands/fun/obsolete.js
@@ -0,0 +1,16 @@
+const { ContextMenuCommandBuilder, ApplicationCommandType } = require('discord.js');
+const path = require('path');
+
+module.exports = {
+  data: new ContextMenuCommandBuilder()
+    .setName('Obsolete')
+    .setType(ApplicationCommandType.Message),
+
+  async execute(interaction) {
+    const imagePath = path.join(__dirname, '../../assets/images/obsolete.png');
+
+    await interaction.reply({
+      files: [imagePath],
+    });
+  },
+};
